4d0b3997db89ad066457df753ec3b10021daf677
hammadraza133 <hammaduser92@gmail.com>
2025-06-24 15:34:35 +0500

    Add files via upload

---
+++ TASK1.py
@@ -0,0 +1,4 @@
+# Task 1: Print Hello World and brief introduction
+
+print("Hello, World!")
+print("My name is Hammad Raza. I'm a student of Robotics and Intelligent Systems at Bahria University. I am currently enrolled in my 5th semester.")
