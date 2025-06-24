f441430ff31cbe0e54a7f64ca5115424525a79e4
hammadraza133 <hammaduser92@gmail.com>
2025-06-24 15:46:57 +0500

    Add files via upload

---
+++ WEEK1_TASK3.py
@@ -0,0 +1,11 @@
+# Task 3: Largest and Smallest among Three Numbers
+
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+c = float(input("Enter third number: "))
+
+largest = max(a, b, c)
+smallest = min(a, b, c)
+
+print(f"The largest number is: {largest}")
+print(f"The smallest number is: {smallest}")
